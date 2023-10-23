09123e916fa0d718952fdce2552bfa710b9a8324
Kaixin Bai <kb@ar-mps01.ar.int>
2023-10-23 14:37:08 +0200

    update

--- models/ops/depthconv/build.py
+++ models/ops/depthconv/build.py
@@ -1,6 +1,44 @@
+# import os
+# import torch
+# from torch.utils.cpp_extension import BuildExtension
+#
+# this_file = os.path.dirname(__file__)
+#
+# sources = ['src/depthconv.c']
+# headers = ['src/depthconv.h']
+# defines = []
+# with_cuda = False
+#
+# if torch.cuda.is_available():
+#     print('Including CUDA code.')
+#     sources += ['src/depthconv_cuda.c']
+#     headers += ['src/depthconv_cuda.h']
+#     defines += [('WITH_CUDA', None)]
+#     with_cuda = True
+#
+# this_file = os.path.dirname(os.path.realpath(__file__))
+# print(this_file)
+# extra_objects = ['src/depthconv_cuda_kernel.cu.o']
+# extra_objects = [os.path.join(this_file, fname) for fname in extra_objects]
+#
+# ffi = BuildExtension(
+#     '_ext.depthconv',
+#     headers=headers,
+#     sources=sources,
+#     define_macros=defines,
+#     relative_to=__file__,
+#     with_cuda=with_cuda,
+#     extra_objects=extra_objects
+# )
+#
+# if __name__ == '__main__':
+#     assert torch.cuda.is_available(), 'Please install CUDA for GPU support.'
+#     ffi.build()
+
 import os
 import torch
-from torch.utils.ffi import create_extension
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 this_file = os.path.dirname(__file__)
 
@@ -21,16 +59,20 @@ print(this_file)
 extra_objects = ['src/depthconv_cuda_kernel.cu.o']
 extra_objects = [os.path.join(this_file, fname) for fname in extra_objects]
 
-ffi = create_extension(
-    '_ext.depthconv',
-    headers=headers,
-    sources=sources,
-    define_macros=defines,
-    relative_to=__file__,
-    with_cuda=with_cuda,
-    extra_objects=extra_objects
+ext_modules = [
+    CUDAExtension(
+        '_ext.depthconv',
+        sources=sources,
+        define_macros=defines,
+        extra_objects=extra_objects,
+    )
+]
+
+setup(
+    name='depthconv',
+    ext_modules=ext_modules,
+    cmdclass={
+        'build_ext': BuildExtension
+    }
 )
 
-if __name__ == '__main__':
-    assert torch.cuda.is_available(), 'Please install CUDA for GPU support.'
-    ffi.build()
